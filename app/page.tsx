d634dc95a6e5eb37d16ae7f669a32f6b3847ec62
Adii <itsaditya101@gmail.com>
2026-02-01 19:08:00 +0530

    added navbar and herosection

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { redirect } from 'next/navigation'
 import React from 'react'
 import User from './models/user.model'
 import Nav from '@/components/Nav'
+import DeliveryBoyDashboard from '@/components/DeliveryBoyDashboard'
+import AdminDashboard from '@/components/AdminDashboard'
+import UserDashboard from '@/components/UserDashboard'
 
 const home = async () => {
   await dbConnect()
@@ -17,10 +20,14 @@ const home = async () => {
   if (inCompleteProfile) {
     return <EditRoleMobile />
   }
+  const plainUser = JSON.parse(JSON.stringify(user))
 
   return (
     <div>
-      <Nav />
+      <Nav user={plainUser} />
+      {user.role == 'user' && <UserDashboard />}
+      {user.role == 'admin' && <AdminDashboard />}
+      {user.role == 'deliveryBoy' && <DeliveryBoyDashboard />}
     </div>
   )
 }
