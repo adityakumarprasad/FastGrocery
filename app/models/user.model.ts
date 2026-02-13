b2c49b3bd4a3262e4a93513f0e001210858b2bdb
Adii <itsaditya101@gmail.com>
2026-02-13 13:34:48 +0530

    working on admin section

--- app/models/user.model.ts
+++ app/models/user.model.ts
@@ -1,26 +1,79 @@
 import mongoose from "mongoose";
-interface IUser {
-  _id: mongoose.Types.ObjectId;
-  name: string;
-  email: string;
-  password?: string;
-  mobile?: string;
-  role: 'user' | 'admin' | 'deliveryboy';
-  image?: string;
+
+export interface IUser {
+  _id?: mongoose.Types.ObjectId
+  name: string
+  email: string
+  password?: string
+  mobile?: string
+  role: "user" | "deliveryBoy" | "admin"
+  image?: string,
+  location?: {
+    type: {
+      type: StringConstructor;
+      enum: string[];
+      default: string;
+    };
+    coordinates: {
+      type: NumberConstructor[];
+      default: number[];
+    };
+  },
+  socketId: string | null
+  isOnline: Boolean
+
 }
 
-const userSchmema = new mongoose.Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: false },
-  mobile: { type: String, required: false },
-  role: { type: String, enum: ['user', 'admin', 'deliveryboy'], default: 'user' },
-  image: { type: String }
-},
-  { timestamps: true }
-);
-const User = mongoose.models.User || mongoose.model<IUser>('User', userSchmema);
+const userSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: true
 
-export default User;
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: false
+  },
+  mobile: {
+    type: String,
+    required: false
+  },
+  role: {
+    type: String,
+    enum: ["user", "deliveryBoy", "admin"],
+    default: "user"
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point"
+    },
+    coordinates: {
+      type: [Number],
+      default: [0, 0]
+    }
+  },
+  socketId: {
+    type: String,
+    default: null
+  },
+  isOnline: {
+    type: Boolean,
+    default: false
+  }
 
+}, { timestamps: true })
 
+userSchema.index({ location: "2dsphere" })
+
+const User = mongoose.models.User || mongoose.model("User", userSchema)
+export default User
